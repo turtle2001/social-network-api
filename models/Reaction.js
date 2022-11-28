8f863e7f90606db4b349136dac168ca931325118
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-11-28 15:59:15 -0800

    thought model

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const { Schema, Types } = require('mongoose');
+const { moment } = require('moment');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      max: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (created) => moment(created).format('MMM DD, YYYY [at] hh:mm:a'),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
